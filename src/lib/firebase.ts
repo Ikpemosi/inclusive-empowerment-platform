121183e08a0f5e0de17f890fd3220f825005b609
Ikpemosi <ikpemosiArnold@outlook.com>
2025-02-19 17:31:38 +0100

    changed config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA56evcRviGI6nO87XAKyIDKwus-LiwyX0",
-  authDomain: "idea-ngo-dev.firebaseapp.com",
-  projectId: "idea-ngo-dev",
-  storageBucket: "idea-ngo-dev.firebasestorage.app",
-  messagingSenderId: "564861791185",
-  appId: "1:564861791185:web:4fa7db151689f82f038ef0",
-  measurementId: "G-B9KF60R9VH"
+  apiKey: "AIzaSyD_gQYhh9lYMERbd0odUSbJIJbLifSAD1g",
+  authDomain: "motiwa-online-tests.firebaseapp.com",
+  databaseURL: "https://motiwa-online-tests-default-rtdb.firebaseio.com",
+  projectId: "motiwa-online-tests",
+  storageBucket: "motiwa-online-tests.appspot.com",
+  messagingSenderId: "522502389732",
+  appId: "1:522502389732:web:224fb4f5f1f5e890a051f1",
+  measurementId: "G-FN2NGD1MM5"
 };
 
 const app = initializeApp(firebaseConfig);
